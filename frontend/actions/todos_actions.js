ec777cc194d9755e9d484078e82f9735a22b405f
Jonathan Berzon <jonathanlberzon@gmail.com>
2021-10-19 14:28:23 -0400

    finished phase 2

--- frontend/actions/todos_actions.js
+++ frontend/actions/todos_actions.js
@@ -1,5 +1,6 @@
 export const RECEIVE_TODOS = "RECEIVE_TODOS";
 export const RECEIVE_TODO = "RECEIVE_TODO";
+export const REMOVE_TODO = "REMOVE_TODO"
 
 // action creators => send array to reducer
 export const receiveTodos = (todos) => ({
@@ -11,4 +12,9 @@ export const receiveTodo = (todo) => ({
   type: RECEIVE_TODO,
   todo, // todo: todo
 });
+
+export const removeTodo = (todo) =>({
+  type: REMOVE_TODO,
+  todo
+})
 
--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,11 +1,39 @@
-import { RECEIVE_TODOS, RECEIVE_TODO } from "../actions/todos_actions";
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from "../actions/todos_actions";
 
-const todosReducer = (state = {}, action) => {
+const initialState = {
+  1: {
+    id: 1,
+    title: "wash car",
+    body: "with soap",
+    done: false
+  },
+  2: {
+    id: 2,
+    title: "wash dog",
+    body: "with shampoo",
+    done: true
+  }
+};
+
+const todosReducer = (state = initialState, action) => {
     Object.freeze(state); // makes sure that the state is immutable
-    const nextState = Object.assign({}, state); // Makes shallow copy of state
+    let nextState = Object.assign({}, state); // Makes shallow copy of state
+    // debugger
   switch (action.type) {
-    // case RECEIVE_TODOS:
+    case RECEIVE_TODOS:
+      nextState = {};
+      action.todos.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
 
+      return nextState;
+    case RECEIVE_TODO:
+      nextState[action.todo.id] = action.todo;
+      return nextState;
+
+    case REMOVE_TODO:
+      delete nextState[action.todo.id];
+      return nextState;
     default:
       return state;
   }
